74387a798c77cc53a44453cec17256a31fa9ac42
Sofia Ribeiro <sribeiro@ubiwhere.pt>
2018-11-20 23:06:40 +0000

    new

--- app/src/main/java/com/example/soalr/myapplication/MyView.java
+++ app/src/main/java/com/example/soalr/myapplication/MyView.java
@@ -120,11 +120,10 @@ public class MyView extends View {
         }
 
         if (figure == 2) {
-            if (ptsReta.size() > 1) {
+            if (ptsReta.size() %2 == 0) {
                 for (int b = 0; b < ptsReta.size(); b = b + 2) {
                     Reta retinha = new Reta(ptsReta.get(b), ptsReta.get(b + 1));
                     guardaRetas.add(retinha);
-                    System.out.println("pts: " + ptsReta.get(b) + ptsReta.get(b + 1));
                 }
 
                 if (guardaRetas.size() > 0) {
